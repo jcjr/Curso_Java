e7dfae988c1b8e9d02a4af80a2d154802f25f8fd
José Carlos Júnior <josecarlosjr.atc@gmail.com>
2023-08-11 21:59:42 -0300

    Adicionado exercício de 15 à 20

--- Exercicio_13/src/PlanoCartesiano.java
+++ Exercicio_13/src/PlanoCartesiano.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 
 public class PlanoCartesiano {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		double x, y;
-		
-		x = sc.nextDouble();
-		y = sc.nextDouble();
-		
-		if(x > 0.0 && y > 0.0) { 
-			System.out.println("Quadrante Q1");
-		} else if(x < 0.0 && y > 0.0) {
-				System.out.println("Quadrante Q2");
-		} else if(x < 0.0 && y < 0.0) {
-				System.out.println("Quadrante Q3");
-		} else if(x > 0.0 && y < 0.0) {
-				System.out.println("Quadrante Q4");
-		} else if(x == 0.0 && y != 0.0) {
-				System.out.println("Eixo Y");
-		} else if(x != 0.0 && y == 0.0) {
-			System.out.println("Eixo X");
-		}	else if(x == 0.0 && y == 0.0) {
-			System.out.println("Na origem");
-		}
-		sc.close();
-	}
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    double x, y;
+
+    x = sc.nextDouble();
+    y = sc.nextDouble();
+
+    if (x > 0.0 && y > 0.0) {
+      System.out.println("Quadrante Q1");
+    } else if (x < 0.0 && y > 0.0) {
+      System.out.println("Quadrante Q2");
+    } else if (x < 0.0 && y < 0.0) {
+      System.out.println("Quadrante Q3");
+    } else if (x > 0.0 && y < 0.0) {
+      System.out.println("Quadrante Q4");
+    } else if (x == 0.0 && y != 0.0) {
+      System.out.println("Eixo Y");
+    } else if (x != 0.0 && y == 0.0) {
+      System.out.println("Eixo X");
+    } else if (x == 0.0 && y == 0.0) {
+      System.out.println("Na origem");
+    }
+    sc.close();
+  }
 
 }
