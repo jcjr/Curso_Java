e98295ee4177489a5a21194f3022b584a469f8d5
José Carlos Jr <josecarlosjr.atc@gmail.com>
2023-12-10 15:03:37 -0300

    Atualizado mais uma vez o exercício 153

--- Exercicio_153/src/application/Heranca.java
+++ Exercicio_153/src/application/Heranca.java
@@ -8,6 +8,7 @@ public class Heranca {
 
 	public static void main(String[] args) {
 		
+		// Primeira parte do exercício
 		Account acc = new Account(1001, "Alex", 0.00);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
@@ -41,7 +42,21 @@ public class Heranca {
 			acc5.updateBalance();
 			System.out.println("Update!");
 		}
+		System.out.println();
+		System.out.println();
 		
+		// Segunda parte do exercício
+		Account acc6 = new Account(1006, "Joao", 1000.0);
+		acc6.withdraw(200.0);
+		System.out.println(acc6.getBalance());
+		
+		Account acc7 = new SavingsAccount(1007, "Pedro", 1000.0, 0.01);
+		acc7.withdraw(200.0);
+		System.out.println(acc7.getBalance());
+		
+		Account acc8 = new BusinessAccount(1008, "Henrique", 1000.0, 500.0);
+		acc8.withdraw(200.0);
+		System.out.println(acc8.getBalance());
 	}
 
 }
