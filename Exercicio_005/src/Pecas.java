f2ad20046729cad077ba7a48a92eda469e50c030
José Carlos Jr <josecarlosjr.atc@gmail.com>
2024-01-13 20:29:14 -0300

    Organizando arquivos no repositório

--- Exercicio_005/src/Pecas.java
+++ Exercicio_005/src/Pecas.java
@@ -5,14 +5,11 @@ public class Pecas {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int cod, cod2, num, num2;
 		double valor, valor2, total;
 		
-		cod = sc.nextInt();
-		num = sc.nextInt();
+		sc.nextInt();
 		valor = sc.nextDouble();
-		cod2 = sc.nextInt();
-		num2 = sc.nextInt();
+		sc.nextInt();
 		valor2 = sc.nextDouble();
 		total = valor + valor2;
 		
