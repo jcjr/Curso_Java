0f83297bc6ba571aac735774e7b9474059e81914
José Carlos Jr <josecarlosjr.atc@gmail.com>
2024-01-13 21:47:29 -0300

    Adicionado exercício 220

---
+++ Exercicio_220/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = null;
+		List<Product> list = new ArrayList<>();
+
+		File file = new File("c:\\temp\\in.txt");
+
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				String[] word = sc.nextLine().split(",");
+				Product prod = new Product(word[0], Double.parseDouble(word[1]), Integer.parseInt(word[2]));
+				list.add(prod);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Copiado lista de produtos para memória.");
+		}
+		
+		if (new File("c:\\temp\\out").mkdir()) {
+			
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter("c:\\temp\\out\\summary.csv", true))) {
+				for (Product ls : list) {
+					bw.write(ls.toString());
+					bw.newLine();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				System.out.println("Criada lista de produtos com sucesso.");
+			}
+		}
+		sc.close();
+	}
+
+}
