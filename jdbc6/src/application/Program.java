d9252f53dbf92289e76d5bdc3e45a0247b48340f
José Carlos Júnior <josecarlosjr.atc@gmail.com>
2024-02-23 11:45:20 -0300

    Atualização do jdbc6

--- jdbc6/src/application/Program.java
+++ jdbc6/src/application/Program.java
@@ -23,20 +23,22 @@ public class Program {
 			st = conn.createStatement();
 			
 			int rows1 = st.executeUpdate("UPDATE seller SET "
-					+ "BaseSalary = 2090 WHERE DepartmentId = 1");
+					+ "BaseSalary = 5230 WHERE DepartmentId = 1");
 			
 			/*int x = 1;
 			if (x < 2) {
 				throw new SQLException("Fake error!!!");
-			}*/
+			}//*/
 			
+			/*
 			int rows2 = st.executeUpdate("UPDATE seller SET "
 					+ "BaseSalary = 3090 WHERE DepartmentId = 2");
-					
+			//*/
+			
 			conn.commit();
 			
 			System.out.println("Done! Rows effected: " + rows1);
-			System.out.println("Done! Rows effected: " + rows2);
+			//System.out.println("Done! Rows effected: " + rows2);
 			
 		} catch (SQLException e) {
 			
